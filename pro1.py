1ee6583fd8e0e38a9c8c97713d48dd36aa141ccc
AbhinavKrishn0 <5fortoxicgamer@gmail.com>
2024-10-27 21:29:56 +0300

    Final code

--- pro1.py
+++ pro1.py
@@ -6,8 +6,8 @@ if a == 'Addition':
 elif a == 'Subtraction':
     print((b - c))
 elif a == 'Multiplication':
-    print= (str(b * c))
+    print((b * c))
 elif a == 'Division':
-    print= (str(b / c))
+    print((b / c))
 else:
     print('Invalid Input')
