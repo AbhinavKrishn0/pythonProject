3e7500753b531aaa6b222a33673f6cfd18f02388
AbhinavKrishn0 <5fortoxicgamer@gmail.com>
2024-10-27 21:27:47 +0300

    new code

---
+++ pro1.py
@@ -0,0 +1,13 @@
+a = input('Select Operation ?')
+b = int(input('> '))
+c = int(input('> '))
+if a == 'Addition':
+    print(int(b + c))
+elif a == 'Subtraction':
+    print((b - c))
+elif a == 'Multiplication':
+    print= (str(b * c))
+elif a == 'Division':
+    print= (str(b / c))
+else:
+    print('Invalid Input')
